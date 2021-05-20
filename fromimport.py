5fda2e61b7224d912d97d06d8abb0c2f6250b7ff
Divana Nayumi Lucky Pratama <divanayumi13@gmail.com>
2021-05-20 21:17:33 +0700

    Pengerjaan modul

---
+++ fromimport.py
@@ -0,0 +1,14 @@
+#mengimpor fungsi luasPersegiPanjang
+#dari modul geometri2D
+from matematika.geometri2D import luasPersegiPanjang
+
+#persegi panjang
+p = 10
+l = 8
+
+luas = luasPersegiPanjang(p,l)
+
+print("Persegi Panjang")
+print("Panjang\t:", p)
+print("Lebar\t:", l)
+print("luas\t:", luas)
